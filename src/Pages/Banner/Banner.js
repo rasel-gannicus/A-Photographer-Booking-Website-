f18b8ccddfd189b84f97fde41c475e473ddc1376
shafik720 <shafikrasel5@gmail.com>
2023-05-24 14:38:49 +0600

    banner section done

---
+++ src/Pages/Banner/Banner.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Banner.css';
+import bannerImg from '../../assets/img/banner-bg.png';
+
+const Banner = () => {
+    return (
+        <div className="banner-div">
+            <div className='container'>
+                <div className="row g-3">
+                    <div className="col-lg-6">
+                        <div className="h-100">
+                            <div className="banner-left h-100">
+                                <div className="banner-left-header">
+                                    <p>Hi There</p>
+                                    <h1><span className="special-color"></span>I am Jonathan Wick</h1>
+                                </div>
+                                <p>A Professional Award Winning PhotoGrapher</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="h-100">
+                            <div className="banner-right">
+                                <div className="banner-img">
+                                    <img src={bannerImg} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import './Home.css' ; 
 
 const Home = () => {
     return (
-        <div>
-            <h2>Hello from home</h2>
+        <div  className='home-div app-div'>
+            <Banner></Banner>
         </div>
     );
 };
