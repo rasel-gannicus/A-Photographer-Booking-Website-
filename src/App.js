e513494743cd790d5c0eba1ddf609b4558a11f94
shafik720 <shafikrasel5@gmail.com>
2023-05-26 20:36:23 +0600

    profile section : User updating functionality done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import Routes from './Routes/Routes';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className='App'>
       <RouterProvider router={Routes}></RouterProvider>
+      <ToastContainer /> 
     </div>
   );
 }
--- src/Pages/Profile/ProfileDetails/ProfileDetails.js
+++ src/Pages/Profile/ProfileDetails/ProfileDetails.js
@@ -1,24 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../../Utilities/firebase.init';
 import './ProfileDetails.css' ; 
+import {ClipLoader} from 'react-spinners'
+import { toast } from 'react-toastify';
 
 const ProfileDetails = () => {
     const [user, loading, error] = useAuthState(auth);
-    const{displayName, email:userEmail, photoURL, } = user;
+    const{displayName : currentUserName, email:userEmail, photoURL, } = user;
+    const [updateProfile, updating,] = useUpdateProfile(auth);
     
     // --- to fill up the input field with user info
-    const [username, setUsername] = useState(displayName);
+    const [username, setUsername] = useState(currentUserName);
     const[email, setEmail] = useState(userEmail);
     const[photo, setPhoto] = useState(photoURL);
 
+    // --- changing the 'Update' button's style
     const[isChanged, setIsChanged] = useState(false);
-    
+    let btnLoader = <ClipLoader color="black" size={20} />
     useEffect(()=>{
-        if(username !== displayName || photo !== photoURL){
+        if(username !== currentUserName || photo !== photoURL){
             setIsChanged(true);
         }
-    },[username, photo, photoURL, displayName])
+    },[username, photo, photoURL, currentUserName]);
+
+    // --- update user functionality
+    const handleUpdateUser = async() => {
+        const isConfirm = window.confirm('Update Profile ?');
+        if(isConfirm){
+            const result = await updateProfile({displayName : username, photoURL:photo});
+            if(result){
+                toast.success('User updated successfully !', {
+                    position: "bottom-center",
+                    autoClose: 1000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                });
+            
+                setIsChanged(false);
+            }
+        }
+    }
+
     return (
         <div className='profile-details'>
             <img src={photo} alt="" />
@@ -38,7 +65,7 @@ const ProfileDetails = () => {
                 <tr>
                     <td></td>
                     <td>
-                        <button disabled={!isChanged} className={isChanged ? 'profile-btn-active' : 'profile-btn-deactive'}>Update</button>
+                        <button onClick={handleUpdateUser} disabled={!isChanged || updating} className={isChanged ? 'profile-btn-active' : 'profile-btn-deactive'}> { updating && btnLoader} Update</button>
                     </td>
                 </tr>
             </table>
