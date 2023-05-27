fb5cc6445b4780ad6a82fba3f83b03d3a0d30e64
shafik720 <shafikrasel5@gmail.com>
2023-05-27 09:48:47 +0600

    initiating mongodb

---
+++ src/Redux/Features/product/productApi.js
@@ -0,0 +1,13 @@
+import { apiSlice } from "../api/apiSlice";
+
+
+
+export const productApi = apiSlice.injectEndpoints({
+    endpoints : (builder)=>({
+
+    })
+})
+
+
+
+export const {} = productApi ; 
