b56ebeb0a6870a5e461e9cb881e28018a673885d
shafik720 <shafikrasel5@gmail.com>
2023-05-28 12:02:25 +0600

    Product Show Section : in home page showing product in my shop section fetched from mongodb done

--- src/Pages/Home/MyShop/MyShop.js
+++ src/Pages/Home/MyShop/MyShop.js
@@ -16,11 +16,13 @@ const MyShop = (props) => {
     // --- deciding what to render while fetching data from server
     let content = null;
 
+    // --- when fetching-data process is in loading state
     if (isLoading && !isError) {
         console.log('Loading');
         content = <div className="loader-in-middle2"><ClipLoader color="black" size={70} /></div>
     }
-
+    
+    // --- when there is a error happened while fetching-data 
     if (!isLoading && isError) {
         console.log(error);
         content = <div className="error-text">
@@ -28,6 +30,7 @@ const MyShop = (props) => {
         </div>;
     }
 
+    // --- when we finally get the data
     if (!isLoading && !isError && data.length > 0) {
         content = <div className="product-show-div ">
             {
@@ -44,12 +47,16 @@ const MyShop = (props) => {
     function loadMoreProduct() {
         setCount(count + 3);
     }
+    function loadLess() {
+        setCount(count - 3);
+    }
     return (
         <div className='my-5 home-my-shop'>
             <h2 className='my-5'>Some Of My Product <br />You Can Buy To Decorate Your Home/Office Wall</h2>
             {content}
             {/* <p className='loading-text mt-5'><Spinner animation="border" variant="primary" /></p> */}
             <button className='loadMoreButton' onClick={loadMoreProduct}>Load More</button>
+            {data?.length>3 && <button className='loadMoreButton' onClick={loadLess}>Load Less</button>}
         </div>
     );
 };
