4b2487375bb64e505f7f6649ee43eccbe330eca0
shafik720 <shafikrasel5@gmail.com>
2023-05-31 18:17:03 +0600

    Shop Section : adding to cart done

--- src/Redux/Features/product/productApi.js
+++ src/Redux/Features/product/productApi.js
@@ -3,17 +3,17 @@ import { apiSlice } from "../api/apiSlice";
 
 
 export const productApi = apiSlice.injectEndpoints({
-    endpoints : (builder)=>({
-        getAllProduct : builder.query({
-            query : (count) => `/product?amount=${count}`
+    endpoints: (builder) => ({
+        getAllProduct: builder.query({
+            query: (count) => `/product?amount=${count}`
         }),
-        getProductByCatagory : builder.mutation({
-            query : ({category, currentPage, contentPerPage}) => ({
-                url : `/product/category/${category}?currentPage=${currentPage}&size=${contentPerPage}`,
-                method : 'POST',
+        getProductByCatagory: builder.mutation({
+            query: ({ category, currentPage, contentPerPage }) => ({
+                url: `/product/category/${category}?currentPage=${currentPage}&size=${contentPerPage}`,
+                method: 'POST',
             }),
             async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-                
+
             }
         }),
 
@@ -21,27 +21,37 @@ export const productApi = apiSlice.injectEndpoints({
             Product Cart Related
         ------------------------------------------------ */
 
-        addProductToCart : builder.mutation({
-            query : (data) => ({
-                url : '/cart/addProduct',
-                method : 'POST',
-                body : {data}
+        addProductToCart: builder.mutation({
+            query: (data) => ({
+                url: '/cart/addProduct',
+                method: 'POST',
+                body: { data }
             }),
-            // async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-            //     console.log('From Arg: ',arg);
-            //     const pathResult = await dispatch(apiSlice.util.updateQueryData('gettingSingleProductFromCart', ({email:arg.email, id:arg.product._id}), (draft)=>{                    
-            //         // selectedProduct
-            //         draft._id = arg;
-            //     } ))
-            // }
-        }),
-        getAllProductCart : builder.query({
-            query : () => '/cart/getAllProduct'
-    }),
+            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+                
+                // const pathResult = await dispatch(apiSlice.util.updateQueryData('gettingSingleProductFromCart', ({email:arg.email, id:arg.product._id}), (draft)=>{                    
+                //     // selectedProduct
+                //     draft._id = arg;
+                // } ))
+                const pathResult2 = await dispatch(apiSlice.util.updateQueryData('getAllProductCart', undefined, (draft)=>{
+                    draft.push(arg);
+                }))
 
-    gettingSingleProductFromCart : builder.query({
-        query : ({email, id}) => `/cart/singleProduct?email=${email}&id=${id}`
-    })
+                try{
+                    const response = await queryFulfilled;
+                }catch(err){
+                    console.log(err);
+                    pathResult2.undo();
+                }
+            }
+        }),
+        getAllProductCart: builder.query({
+            query: () => '/cart/getAllProduct'
+        }),
+
+        gettingSingleProductFromCart: builder.query({
+            query: ({ email, id }) => `/cart/singleProduct?email=${email}&id=${id}`
+        })
 
 
     })
@@ -49,4 +59,4 @@ export const productApi = apiSlice.injectEndpoints({
 
 
 
-export const {useGetAllProductQuery, useGetProductByCatagoryQuery, useGetProductByCatagoryMutation, useAddProductToCartMutation, useGetAllProductCartQuery, useGettingSingleProductFromCartQuery} = productApi ; 
+export const { useGetAllProductQuery, useGetProductByCatagoryQuery, useGetProductByCatagoryMutation, useAddProductToCartMutation, useGetAllProductCartQuery, useGettingSingleProductFromCartQuery } = productApi; 
--- src/Utilities/CartPopup/CartPopup.js
+++ src/Utilities/CartPopup/CartPopup.js
@@ -5,6 +5,7 @@ import { useGetServiceCartQuery } from '../../Redux/Features/service/serviceApi'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../firebase.init';
 import { useNavigate } from 'react-router-dom';
+import { useGetAllProductCartQuery } from '../../Redux/Features/product/productApi';
 
 const CartPopup = () => {
     // --- taking user to corresponding cart details page
@@ -17,13 +18,22 @@ const CartPopup = () => {
 
     // --- getting user cart info for service booking
     const { data } = useGetServiceCartQuery(user?.email);
-    // console.log(data);
+    
+    // --- getting user's product cart info
+    const{data:products} = useGetAllProductCartQuery();
+    // console.log(products);
     return (
         <div className='mini-popup'>
             <div onClick={()=>navigation('/profile/bookings')} draggable className="booking-cart">
                 {data?.length>0 && <div className="booking-cart-child">
                     <img src="https://i.ibb.co/BGLHLcM/clock.png" alt="" />
                     <span><p>{data?.length}</p></span>
+                </div>}
+            </div>
+            <div onClick={()=>navigation('/profile/cart')} draggable className="products-cart">
+                {products?.length>0 && <div className="booking-cart-child">
+                    <img src="https://i.ibb.co/9b9NP7G/shopping-cart-2.png" alt="" />
+                    <span><p>{products?.length}</p></span>
                 </div>}
             </div>
         </div>
