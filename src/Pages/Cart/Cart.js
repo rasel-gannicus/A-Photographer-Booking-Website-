0f3872d15c0282d44cbb53a9d3f9c409a7c02a9f
shafik720 <shafikrasel5@gmail.com>
2023-05-31 19:16:55 +0600

    Cart Section : styling card

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const Cart = () => {
     return (
-        <div>
-            <h2>Cart Here</h2>
+        <div className='cart-div'>
+            <div className="cart-cards">
+                <div className="first-half">
+                    <div className="first-half-img">
+                        <img src="https://i.ibb.co/7kpLW1Y/street-11.jpg" alt="" />
+                    </div>
+                    {/* <div className="first-half-details">
+                        <h2>Product Title</h2>
+                        <p>Catagory : Wild</p>
+                    </div> */}
+                </div>
+                <div className="second-half">
+                    <div className="amount-div">
+                        <div className="amount-icon">
+                            <span><FontAwesomeIcon icon={faMinus} /></span>
+                            <p>5</p>
+                            <span><FontAwesomeIcon icon={faPlus} /></span>
+                        </div>
+                    </div>
+                    <div className="total-price-div">
+                        <p>Total : $ 2500</p>
+                    </div>
+                </div>
+                {/* <div className="third-half">
+                    <button>Confirm Order</button>
+                </div> */}
+            </div>
         </div>
     );
 };
