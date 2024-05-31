09005d5ed9b320736284891b44de20d1de97ebf9
shafik720 <shafikrasel5@gmail.com>
2024-06-01 01:41:45 +0600

    successfully created custom made modal

--- src/Redux/Features/api/apiSlice.js
+++ src/Redux/Features/api/apiSlice.js
@@ -1,13 +1,11 @@
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
 export const apiSlice = createApi({
-    reducerPath : 'api',
-    baseQuery : fetchBaseQuery({
-        // baseUrl : process.env.REACT_APP_API_URL,
-        baseUrl : 'http://localhost:5004',
-    }),
-    tagTypes : [],
-    endpoints : (builder) => ({})
-})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_API_URL,
+    // baseUrl : 'http://localhost:5004',
+  }),
+  tagTypes: [],
+  endpoints: (builder) => ({}),
+});
--- src/Utilities/Modal/Modals2.js
+++ src/Utilities/Modal/Modals2.js
@@ -5,7 +5,8 @@ import { hideModal } from "../../Redux/Features/modal/modalSlice";
 import { useGetSingleProductQuery } from "../../Redux/Features/product/productApi";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark , faHeartCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { faXmark, faHeartCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { ClipLoader } from "react-spinners";
 
 const Modals2 = () => {
   const modalDetails = useSelector((state) => state.modal);
@@ -20,8 +21,65 @@ const Modals2 = () => {
   const dispatch = useDispatch();
 
   //   --- getting single product details from mongodb using redux
-    const { data } = useGetSingleProductQuery(dataId);
-    console.log(data);
+  const { data, isLoading, isError, error, isSuccess } =
+    useGetSingleProductQuery(dataId);
+  // --- deciding what to show in UI while fetching data from server
+  let content = null;
+
+  // --- when fetching-data process is in loading state
+  if (isLoading && !isError) {
+    content = (
+      <div className="loader-in-middle2  mt-5">
+        <ClipLoader color="black" size={70} />
+      </div>
+    );
+  }
+
+  // --- when there is a error happened while fetching-data
+  if (!isLoading && isError) {
+    console.log(error);
+    content = <div className="error-text">{error?.error}</div>;
+  }
+
+  // --- when we finally get the data
+  if (!isLoading && !isError && isSuccess) {
+    content = (
+      <div className="modals-2-innerBody">
+        <div className="modal-left-side">
+          <div className="product-img-parent">
+            <div className="product-img">
+              <img src={data?.img} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="modal-right-side">
+          <div>
+            <p>Category : </p>
+            <p>{data?.catagory} Photography </p>
+          </div>
+          <div>
+            <p>Price : </p>
+            <p> $ {data?.price} </p>
+          </div>
+          <div>
+            <p>Description : </p>
+            <p> .... </p>
+          </div>
+          <div className="modal-button-section">
+            <button>Buy Now</button>
+            <button>
+              {" "}
+              <FontAwesomeIcon
+                icon={faHeartCirclePlus}
+                style={{ marginRight: "10px" }}
+              />
+              Favourite
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={`modals-2 ${modalStatus && "active"}`}>
@@ -32,35 +90,7 @@ const Modals2 = () => {
           </button>
           <h5>Product Details </h5>
         </div>
-        <div className="modals-2-innerBody">
-          <div className="modal-left-side">
-
-            <div className="product-img-parent">
-              <div className="product-img">
-                <img src={data.img} alt="" />
-              </div>
-            </div>
-
-          </div>
-          <div className="modal-right-side">
-            <div>
-                <p>Category : </p>
-                <p>{data.catagory} Photography </p>
-            </div>
-            <div>
-                <p>Price : </p>
-                <p> $ {data.price} </p>
-            </div>
-            <div>
-                <p>Description : </p>
-                <p> .... </p>
-            </div>
-            <div className="modal-button-section">
-                <button>Buy Now</button>
-                <button> <FontAwesomeIcon icon={faHeartCirclePlus} style={{marginRight : '10px'}} />Favourite</button>
-            </div>
-          </div>
-        </div>
+        {content}
         <div className="modals-2-footer"></div>
       </div>
     </div>
