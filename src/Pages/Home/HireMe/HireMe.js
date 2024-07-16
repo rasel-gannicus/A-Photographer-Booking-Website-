986b5a2bc8f4f8dbeaa138fb97b478bde2803a4d
shafik720 <shafikrasel5@gmail.com>
2024-07-16 19:43:20 +0600

    modified hire me section

--- src/Pages/Home/HireMe/HireMe.js
+++ src/Pages/Home/HireMe/HireMe.js
@@ -4,14 +4,14 @@ import './HireMe.css'
 const HireMe = () => {
   const navigate = useNavigate() ;
   return (
-    <div className=" container  hire-me-div py-[300px] md:bg-right text-left">
+    <div className=" container  hire-me-div py-[300px] md:bg-right text-right">
       <h2 className=" text-4xl md:text-8xl text-[#4A4B7C] font-bold">
         Hire Me for Your <br />
         Next Project
       </h2>
       <button
         onClick={() => navigate('/bookNow')}
-        className="px-3 py-3 text-lg mt-10  shadow-2xl bg-[#4A4B7C] text-white rounded font-semibold hover:bg-[#7375c8]   transition-all"
+        className="px-8 py-3 text-xl mt-10  shadow-2xl bg-[#EC228A] text-white rounded font-semibold hover:bg-[#7375c8]   transition-all duration-300 "
       >
         Hire Me
       </button>
